2419a3d77f1336871d6f248c1d466f166b363636
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2023-04-24 20:30:50 -0500

    working on server side, it is recognizing the request

---
+++ assets/js/schedule.js
@@ -0,0 +1,15 @@
+const { response } = require("express");
+
+document.addEventListener('DOMContentLoaded', function (){
+fetch('http://localhost:5000/getAll')
+.then(response => response.json())
+.then(data => console.log(data));
+loadHTMLTable([]);
+});
+
+function loadHTMLTable(data){
+    const table = document.querySelector('table tbody');
+    if (data.length === 0) {
+        table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
+    }
+}
