3f6eeff8d56be86ac12cd4f51b0b99f64f2fcb1a
Liam Allen <liamallen343@gmail.com>
2023-04-26 19:09:08 -0500

    Added service page and started styling for page

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function() {
+
+    $(".large-service-div").click(function() {
+        $(this).find(".large-service-text").toggleClass("service-extend")
+    })  
+
+})
+
+
+    
+
