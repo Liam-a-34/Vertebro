4fe51babfd3370b2a170239de6fb35df5ed68bd9
Liam Allen <liamallen343@gmail.com>
2023-04-27 12:49:56 -0500

    Finished up services page so far

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,12 @@ $(document).ready(function() {
 
     $(".large-service-div").click(function() {
         $(this).find(".large-service-text").toggleClass("service-extend")
+        setTimeout(() => {
+            $(this).find(".large-service-text").toggleClass("service-padding")
+        },250)
+        setTimeout(() => {
+            $(this).toggleClass("service-shadow")
+        },500)
     })  
 
 })
